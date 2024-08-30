3b11fe5afa87b40b3e62be8ffffd6f9223428fb2
HuaiyanRen <928003585@qq.com>
2024-08-30 12:42:52 +1000

    Update collect_trees.py

--- collect_trees.py
+++ collect_trees.py
@@ -1,24 +1,45 @@
 import os
+import csv 
 
-folder_name = '14972_exon_results'
+iqtree_folder = 'aa/'
+output_file = 'astral_trees/aa.txt'
+result_csv = 'astral_trees/aa.csv'
+with open(result_csv,'w+',newline='') as csvf:
+    csv_write = csv.writer(csvf)
+    csv_write.writerow(['name', 'tree_exist'])
 
-folder_path = os.getcwd()
-tree_path = os.path.join(folder_path,folder_name)
-output_file = folder_name + '_trees.txt'
+
+with open("14972_exon_alns_namelist.txt") as f:
+    f_list = f.readlines()
+
+
 
 treefile_list = []
-n = 0
-for file in os.listdir(tree_path):
-    if file.endswith('.treefile'):
+n =0
+for i in range(0,100):
+    loci_name = f_list[i].split(" ")[1].split('.')[0]
+    tree_name = iqtree_folder + str(i) + '_' + f_list[i].split(" ")[1].split('.')[0] +'.treefile'
+        
+    if os.path.isfile(tree_name):
         #file_order = int(file.split('_')[0])
         #if file_order in list1:
-        treefile_list.append(file)
+        treefile_list.append(tree_name)
         n = n + 1
-        #print(file)
+        tree_exist = True
+    else:
+        print(loci_name)
+        tree_exist = False
+        
+    result_row = [loci_name, tree_exist]
+
+    with open(result_csv,'a+',newline='') as csvf:
+        csv_write = csv.writer(csvf)
+        csv_write.writerow(result_row)  
+           
 print(n)
 
 with open(output_file, 'w') as output_file:
     for treefile in treefile_list:
-        treefile_path = os.path.join(tree_path, treefile)
-        with open(treefile_path, 'r') as current_file:
+        with open(treefile, 'r') as current_file:
             output_file.write(current_file.read())
+            
