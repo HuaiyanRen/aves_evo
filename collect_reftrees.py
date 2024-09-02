34e382b3f6d8f1c4608121610fd386d41b72ab4e
HuaiyanRen <928003585@qq.com>
2024-09-02 14:11:29 +1000

    filter unfinished trees

--- collect_reftrees.py
+++ collect_reftrees.py
@@ -1,5 +1,5 @@
 iqtree_folder = 'gene_trees/'
-output_file = 'astral_trees/refgene100.txt'
+output_file = 'astral_trees/refexon86.txt'
 
 
 with open("14972_exon_alns_namelist.txt") as f:
@@ -10,11 +10,12 @@ with open("14972_exon_alns_namelist.txt") as f:
 treefile_list = []
 n =0
 for i in range(0,100):
-    loci_name = f_list[i].split(" ")[1].split('.')[0]
-    tree_name = iqtree_folder  + f_list[i].split(" ")[1].split('.')[0] +'.treefile'
+    if i not in [3, 9, 11, 24, 27, 28, 29, 44, 50, 65, 76, 80, 95, 96]:
+        loci_name = f_list[i].split(" ")[1].split('.')[0]
+        tree_name = iqtree_folder  + f_list[i].split(" ")[1].split('.')[0] +'.treefile'
         
-    treefile_list.append(tree_name)
-    n = n + 1
+        treefile_list.append(tree_name)
+        n = n + 1
 
 print(n)
 
--- collect_trees.py
+++ collect_trees.py
@@ -15,28 +15,33 @@ with open("14972_exon_alns_namelist.txt") as f:
 
 
 treefile_list = []
+exclude_list = []
 n =0
 for i in range(0,100):
-    loci_name = f_list[i].split(" ")[1].split('.')[0]
-    tree_name = iqtree_folder + str(i) + '_' + f_list[i].split(" ")[1].split('.')[0] +'.treefile'
-        
-    if os.path.isfile(tree_name):
-        #file_order = int(file.split('_')[0])
-        #if file_order in list1:
-        treefile_list.append(tree_name)
-        n = n + 1
-        tree_exist = True
-    else:
-        print(loci_name)
-        tree_exist = False
-        
-    result_row = [loci_name, tree_exist]
-
-    with open(result_csv,'a+',newline='') as csvf:
-        csv_write = csv.writer(csvf)
-        csv_write.writerow(result_row)  
+    if i not in [3, 9, 11, 24, 27, 28, 29, 44, 50, 65, 76, 80, 95, 96]:
+        loci_name = f_list[i].split(" ")[1].split('.')[0]
+        iqtree_name = iqtree_folder + str(i) + '_' + f_list[i].split(" ")[1].split('.')[0] +'.iqtree'
+        tree_name = iqtree_folder + str(i) + '_' + f_list[i].split(" ")[1].split('.')[0] +'.treefile'
+            
+        if os.path.isfile(iqtree_name):
+            #file_order = int(file.split('_')[0])
+            #if file_order in list1:
+            treefile_list.append(tree_name)
+            n = n + 1
+            tree_exist = True
+        else:
+            print(loci_name)
+            exclude_list.append(i)
+            tree_exist = False
+            
+        result_row = [loci_name, tree_exist]
+    
+        with open(result_csv,'a+',newline='') as csvf:
+            csv_write = csv.writer(csvf)
+            csv_write.writerow(result_row)  
            
 print(n)
+print(exclude_list)
 
 with open(output_file, 'w') as output_file:
     for treefile in treefile_list:
