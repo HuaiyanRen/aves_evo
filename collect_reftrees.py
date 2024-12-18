986e89182fb29cc4bb45e24e4460d71c64cf37af
HuaiyanRen <928003585@qq.com>
2024-12-18 14:00:52 +1100

    apply pythia

--- collect_reftrees.py
+++ collect_reftrees.py
@@ -1,33 +1,35 @@
+tree_folder = 'exon_trees/'
+output_file = 'astral_trees/ref_f90p.txt'
 
 
-iqtree_folder = 'gene_trees/'
-output_file = 'astral_trees/ref_chr1.txt'
-
-
-with open("chr1_namelist_random.txt") as f:
+with open("exon_namelist_random.txt") as f:
     f_list = f.readlines()
 
 
 name_list = []
 treefile_list = []
 n =0
-for i in range(0,1266):
-    #if i not in [3, 9, 11, 24, 27, 28, 29, 44, 50, 65, 76, 80, 95, 96]:
-        loci_name = f_list[i].split(" ")[1].split('.')[0]
-        #tree_name = iqtree_folder  + loci_name +'.treefile'
-        
-        #treefile_list.append(tree_name)
-        name_list.append(loci_name)
-        #n = n + 1
+for i in range(0,14792):
 
-with open('63430.named.gene.trees') as f:
-    for line in f.readlines():
-        if line.split('_1k')[0] in name_list:
-            treefile_list.append(line.split()[1])            
-            n = n + 1
+    loci_name = f_list[i].split(" ")[1].rstrip()
+    
+    
+    pythia_score = 1    
+    with open('rf_compare/'+str(i) + '_' + loci_name + '_pythia.txt') as b:
+        for line in b.readlines():
+            if 'The predicted difficulty for MSA' in line:
+                pythia_score = float(line.split()[-1])
+                
+    if pythia_score < 0.9:
+        tree_name = tree_folder  + loci_name +'.treefile'
+        treefile_list.append(tree_name)
+        n = n + 1
+
+    
 
 print(n)
 
 with open(output_file, 'w') as output_file:
     for treefile in treefile_list:
-        output_file.write(f"{treefile}\n")
+        with open(treefile, 'r') as current_file:
+            output_file.write(current_file.read())
